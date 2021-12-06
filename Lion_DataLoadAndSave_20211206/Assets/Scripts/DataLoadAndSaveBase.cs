016c74275e18c7cd4c09d5a3f74d9e13e8dbd2b1
KID <50666207+KID421@users.noreply.github.com>
2021-12-06 15:11:36 +0800

    Update 完成採礦功能並建立基底存取類別

---
+++ Lion_DataLoadAndSave_20211206/Assets/Scripts/DataLoadAndSaveBase.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 資料存取基底
+    /// 載入資料
+    /// 儲存資料
+    /// </summary>
+    public class DataLoadAndSaveBase : MonoBehaviour
+    {
+        #region 方法：公開並允許複寫
+        /// <summary>
+        /// 儲存資料
+        /// </summary>
+        public virtual void SaveData()
+        {
+
+        }
+
+        /// <summary>
+        /// 載入資料
+        /// </summary>
+        public virtual void LoadData()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Lion_DataLoadAndSave_20211206/Assets/Scripts/GoldManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KID
+{
+    /// <summary>
+    /// 採金礦管理器
+    /// </summary>
+    public class GoldManager : MonoBehaviour
+    {
+        #region 欄位：公開
+        [Header("採礦半徑"), Range(0, 10)]
+        public float radius = 3.5f;
+        [Header("採礦按鍵")]
+        public KeyCode keyGold = KeyCode.E;
+        [Header("目標圖層")]
+        public LayerMask layerTarget;
+        [Header("金幣數量")]
+        public Text textCoin;
+        #endregion
+
+        #region 欄位：私人
+        private int goldCount;
+        #endregion
+
+        #region 屬性：私人
+        /// <summary>
+        /// 按下採礦按鍵
+        /// </summary>
+        private bool inputGold { get => Input.GetKeyDown(keyGold); }
+        /// <summary>
+        /// 目標是否在半徑內
+        /// </summary>
+        private bool targetInRadius { get => Physics2D.OverlapCircle(transform.position, radius, layerTarget); }
+        #endregion
+
+        #region 事件
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = new Color(0.8f, 0.7f, 0.3f, 0.35f);
+            Gizmos.DrawSphere(transform.position, radius);
+        }
+
+        private void Update()
+        {
+            GetGold();
+        }
+        #endregion
+
+        #region 方法：私人
+        /// <summary>
+        /// 採礦
+        /// </summary>
+        private void GetGold()
+        {
+            if (inputGold && targetInRadius)
+            {
+                goldCount++;
+                textCoin.text = "金幣數量：" + goldCount;
+            }
+        }
+        #endregion
+    }
+}
+
